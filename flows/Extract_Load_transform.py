8784fcb3901f5651efdc4f1db8d3fd3ce7b7a44d
ttwange <ttwange2@gmail.com>
2023-09-03 12:08:46 +0300

    working on the extract function of the api

--- flows/Extract_Load_transform.py
+++ flows/Extract_Load_transform.py
@@ -1,18 +1,20 @@
-import requests
 import json
-
+import requests
+import pandas as pd
 
 url = "http://api.coincap.io/v2/assets"
-payload = {}
-headers = {}
-
-response = requests.request("GET",url, headers=headers, data=payload)
-json_data = json.loads(response.text.encode('utf8'))
-
-asset_data = json_data["data"]
-
-import pandas as pd
-df = pd.DataFrame(asset_data)
-df.to_csv('asset-data.csv',index=False)
-
 print(df)
+
+def get_asset_data(url: str) -> pd.DataFrame:
+  """Get asset data from CoinCap API."""
+  payload = {}
+  headers = {}
+  response = requests.request("GET",url, headers=headers, data=payload)
+  json_data = json.loads(response.text.encode('utf8'))
+  asset_data = json_data["data"]
+
+  df = pd.DataFrame(asset_data)
+  df.to_csv('asset-data.csv',index=False)
+  return df
+
+
