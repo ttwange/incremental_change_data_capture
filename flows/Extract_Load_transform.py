ddcceeb337779a895a273802906b002fa47d4093
ttwange <ttwange2@gmail.com>
2023-09-03 11:21:25 +0300

    Managed to pull api data from coin cap api and convert the json file into csv dataframe

--- flows/Extract_Load_transform.py
+++ flows/Extract_Load_transform.py
@@ -1,5 +1,18 @@
 import requests
-import requests
+import json
 
 
-response = 
+url = "http://api.coincap.io/v2/assets"
+payload = {}
+headers = {}
+
+response = requests.request("GET",url, headers=headers, data=payload)
+json_data = json.loads(response.text.encode('utf8'))
+
+asset_data = json_data["data"]
+
+import pandas as pd
+df = pd.DataFrame(asset_data)
+df.to_csv('asset-data.csv',index=False)
+
+print(df)
